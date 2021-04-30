108d86f1a089f3f772fc28ae48f6a059edc1f368
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-30 01:41:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
- if array.length == 2
+ if array.length >= 2
     array.insert(-2, "and")
     array.join
-end
-if array.length > 3
+elsif array.length > 3
   array.join(", ")
+else
+  array.to_s
 end
 end
