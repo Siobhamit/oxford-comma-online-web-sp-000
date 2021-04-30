0190634996b746fc87d88c85363e3fa4cd35d9b7
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-04-30 02:10:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
  if array.length >= 3
-array[-2] << " and"
+  array[-1].prepend(" and")
 array.join(", ")
 elsif array.length == 2
-  array[-2] << " and"
+  array[-1].prepend(" and")
    array.join(" ")
 else
   array.join
